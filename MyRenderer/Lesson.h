7691ed37ffc6b7c5575f8c1206785b78c89cf818
Elmando2333 <1091992414@qq.com>
2021-03-10 11:08:11 +0800

    添加项目文件。

---
+++ MyRenderer/Lesson.h
@@ -0,0 +1,20 @@
+#ifndef __Lesson_H__
+#define __Lesson_H__
+
+#include <cmath>
+#include <cassert>
+#include <iostream>
+#include "tgaimage.h"
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+void line(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color);
+void line1(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color);
+void line2(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color);
+void line3(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color);
+void lineFinal(int x0, int y0, int x1, int y1, TGAImage& image, TGAColor color);
+
+
+
+#endif
